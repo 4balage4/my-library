ffa34af4cba12bb9579831b28645bcc35d48a6c5
balazs burian <bur.balazs@protonmail.com>
2023-01-23 13:10:25 +0100

    book controller

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -6,4 +6,39 @@ class BooksController < ApplicationController
   def show
     @book = Book.find(params[:id])
   end
+
+  def new
+    @book = Book.new
+  end
+
+  def create
+    @book = Book.new(book_params)
+    @book.user_id = current_user.id
+    if @book.save
+      redirect_to books_path
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  def edit
+    @book = Book.find(params[:id])
+  end
+
+  def update
+    @book = Book.find(params[:id])
+    @book.update(book_params)
+  end
+
+  def destroy
+    @book = Book.find(params[:id])
+    @book.destroy
+    redirect_to books_path
+  end
+
+  private
+
+  def book_params
+    params.require(:book).permit(:title, :writer, :year, :language, :genree, :pages, :status, :date_finished, :print, :purchased, :lended, :lended_to, :location)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,10 @@ Rails.application.routes.draw do
   root to: "pages#home"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   get "books", to: "books#index"
+  get "books/new", to: "books#new"
+  post "books", to: "books#create"
   get "books/:id", to: "books#show"
+
   # Defines the root path route ("/")
   # root "articles#index"
 end
